7a4dcd0a1a103c805f3b3ffbf0debb0cb0e85700
petrambo <pet_rambo@hotmail.com>
2019-01-14 01:26:40 +0700

    Calculating grade  is complete

--- CalculateGPA.py
+++ CalculateGPA.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
+
 class main():
     """funtion for import grade from csv and choose data from multi dataframe"""
     def readfile(self):
-     self.readCSV = pd.read_csv("w1_GPA.csv",encoding = "utf8")
-     self.Choosedata = self.readCSV[['ปีการศึกษา','ชื่อวิชา','หน่วยกิต','เกรด']]
+     self.readCSV = pd.read_csv("w1_GPA.csv",encoding = "utf8")  #import file csv.
+     self.Choosedata = self.readCSV[['ปีการศึกษา','ชื่อวิชา','หน่วยกิต','เกรด']] #Select only some active columns
      self.Choosedata = self.Choosedata.iloc[0:]
-     print(self.Choosedata)
+
     def insertgrade(self):
      self.row = input("Select row you want to edit: ")
      self.column = input("Select column you want to edit:" )
@@ -14,6 +15,7 @@ class main():
      self.Choosedata.loc[self.row,self.column] = self.editgrade
      self.Choosedata.to_csv("1.csv")
      print(self.Choosedata)
+
     def editgrade(self):
      self.row1 = input("Select row you want to edit: ")
      self.column1 = input("Select column you want to edit:" )
@@ -21,6 +23,8 @@ class main():
      self.Choosedata.set_value('5', self.column1, self.editgrade1)
      self.Choosedata.to_csv("1.csv")
      print(self.row1)
+
+    """funtion for calculate grade each term"""
     def calcularategrade(self):
      idx = 0
      rategrade = {'A': 4, 'B+': 3.5,'B': 3,'C+': 2.5,'C': 2,'D+': 1.5,'D': 1,'F': 0} #set value for convert grade to numaric
@@ -30,8 +34,23 @@ class main():
      self.Choosedata.columns = ['ปีการศึกษา','ชื่อวิชา','หน่วยกิต','เกรด','น้ำหนักเฉลี่ย']     #rename duplicate column name
      self.Choosedata['นก*เกรด'] = self.Choosedata['หน่วยกิต'] * self.Choosedata['น้ำหนักเฉลี่ย']
      del self.Choosedata['น้ำหนักเฉลี่ย']         #Deleted columns that have not been calculated
-     self.Choosedata.to_csv("1.csv")
-     print(self.Choosedata)
+     self.Choosedata['รวมหน่วยกิต'] = self.Choosedata.groupby(['ปีการศึกษา'])['หน่วยกิต'].transform('sum') #Total for further calculation
+     self.Choosedata['รวมน้ำหนัก'] = self.Choosedata.groupby(['ปีการศึกษา'])['นก*เกรด'].transform('sum')
+     self.Newdata = self.Choosedata.drop_duplicates(subset=['ปีการศึกษา'])   # Create new data for calcalate grade each term
+     del self.Newdata['ชื่อวิชา'] # Delete the calculated columns for the sort of dataframe.
+     del self.Newdata['เกรด']
+     del self.Newdata['นก*เกรด']
+     self.Newdata['เกรดเฉลี่ยต่อเทอม'] = self.Newdata['รวมน้ำหนัก']/self.Newdata['รวมหน่วยกิต'] # Divided to find the average of the grade
+     self.Newdata = self.Newdata.round(2)  # Gives results equal to 2 decimal places
+     del self.Newdata['รวมน้ำหนัก']  # Delete the calculated columns for the sort of dataframe.
+     del self.Newdata['รวมหน่วยกิต']
+     del self.Newdata['หน่วยกิต']
+     del self.Choosedata['รวมหน่วยกิต']
+     del self.Choosedata['รวมน้ำหนัก']
+     del self.Choosedata['นก*เกรด']
+     self.Choosedata = pd.concat([self.Choosedata,self.Newdata], axis=1) # Combine old and new data into the same dataframe
+     self.Choosedata.to_csv("1.csv") # Export dataframe to CSV file
+
 demo = main()
 demo.readfile()
 #demo.editgrade()
